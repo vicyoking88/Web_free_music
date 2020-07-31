1615173edb04d66f08a250482ed3db4970b28229
Victor Medina <vamedinac@icloud.com>
2020-07-30 23:09:00 -0500

    desarrollando reproducciones script 1

--- js/script.js
+++ js/script.js
@@ -7,13 +7,54 @@ function cargarTop3() {
   //funcion que se va a ejecutar cuando los datos lleguen
   peticion.onload = function () {
     if (peticion.status == 200) {
-      // console.log('peticion.response: ', peticion.response);
+      //console.log('peticion.response: ', peticion.response);
       var datos = JSON.parse(peticion.response)
-      // console.log('datos: ', datos);
-      document.getElementById("canciones").innerHTML = "Nombre  : " + datos.canciones.nombre;
+
+      console.log(maxRepro(datos));
+
+      //console.log(datos['canciones'].length);
+      document.getElementById("contenido").innerHTML = "Nombre  : " + datos['canciones'][0]['nombre'];
     }
-  };
+  }
   peticion.send();
 }
 
-cargarTop3();
+
+
+//con esto buscamos la cancion que tiene mas reproducciones
+function maxRepro(datos) {
+  let aRepro = [0, 0, 0, 0, 0, 0];
+  for (var i = 0; i < datos['canciones'].length; i++) {
+    monto = datos['canciones'][i]['reproducciones']
+    if (monto > aRepro[0]) {
+      aRepro[0] = monto;//valor maximo de reproducciones
+      aRepro[1] = i;//cancion con mayor numero de reproducciones
+    } else {
+      if (monto > aRepro[2] && monto < aRepro[0]) {
+        aRepro[2] = monto;//valor maximo de reproducciones
+        aRepro[3] = i;//cancion con mayor numero de reproducciones
+      } else {
+        if (monto > aRepro[4] && monto < aRepro[2]) {
+          aRepro[4] = monto;//valor maximo de reproducciones
+          aRepro[5] = i;//cancion con mayor numero de reproducciones
+        }
+      }
+    }
+  }
+  return aRepro;
+}
+/*
+function max3Repro(datos) {
+  var masRepro = new Object();
+  masRepro.canR=0;
+  for (var i = 0; i < datos['canciones'].length ; i++) {
+    monto = datos['canciones'][i]['reproducciones']
+    if (monto > masRepro['canR']) {
+
+    }
+  }
+  return aRepro;
+
+}
+*/
+
---
+++ pruebas.js
@@ -0,0 +1,3 @@
+let array = [4, 5, 8, 7, 8, 9, 6];
+
+console.log(getMaxOfArray(array));
