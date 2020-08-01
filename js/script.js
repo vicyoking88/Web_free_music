ab98f2017d53c8a2940a902b3eb96b326c42d34b
Victor Medina <vamedinac@icloud.com>
2020-07-31 22:06:38 -0500

    desarrollando reproducciones script 2

--- js/script.js
+++ js/script.js
@@ -9,32 +9,51 @@ function cargarTop3() {
     if (peticion.status == 200) {
       //console.log('peticion.response: ', peticion.response);
       var datos = JSON.parse(peticion.response)
-
-      console.log(maxRepro(datos));
+      //console.log(maxRepro(datos));
+      //cargamos los top 3 en un array
+      var topAll = maxRepro(datos);
 
       //console.log(datos['canciones'].length);
-      document.getElementById("contenido").innerHTML = "Nombre  : " + datos['canciones'][0]['nombre'];
+
+      //cargamos al dom nombre primera cancion
+      document.getElementById("top1").innerHTML = datos['canciones'][topAll[1]]['nombre'];
+      //cargamos al dom la repruduccion de la primera cancion
+      document.getElementById("top1C").innerHTML =
+        "<audio src=" + "songs/" + datos['canciones'][topAll[1]]['ruta'] + " controls></audio>";
+
+      //cargamos al dom nombre segunda cancion
+      document.getElementById("top2").innerHTML = datos['canciones'][topAll[3]]['nombre'];
+      //cargamos al dom la repruduccion de la segunda cancion
+      document.getElementById("top2C").innerHTML =
+        "<audio src=" + "songs/" + datos['canciones'][topAll[3]]['ruta'] + " controls></audio>";
+
+      //cargamos al dom nombre tercera cancion
+      document.getElementById("top3").innerHTML = datos['canciones'][topAll[5]]['nombre'];
+      //cargamos al dom la repruduccion de la tercera cancion
+      document.getElementById("top3C").innerHTML =
+        "<audio src=" + "songs/"+datos['canciones'][topAll[5]]['ruta']+" controls></audio>";
     }
   }
   peticion.send();
 }
 
-
+//cargamos el top3 al cargar la pagina principal
+cargarTop3();
 
 //con esto buscamos la cancion que tiene mas reproducciones
 function maxRepro(datos) {
-  let aRepro = [0, 0, 0, 0, 0, 0];
+  let aRepro = [0, 0, 0, 0, 0, 0];//array que cargamos con las top3
   for (var i = 0; i < datos['canciones'].length; i++) {
     monto = datos['canciones'][i]['reproducciones']
     if (monto > aRepro[0]) {
       aRepro[0] = monto;//valor maximo de reproducciones
       aRepro[1] = i;//cancion con mayor numero de reproducciones
     } else {
-      if (monto > aRepro[2] && monto < aRepro[0]) {
+      if (monto > aRepro[2] && monto <= aRepro[0]) {
         aRepro[2] = monto;//valor maximo de reproducciones
         aRepro[3] = i;//cancion con mayor numero de reproducciones
       } else {
-        if (monto > aRepro[4] && monto < aRepro[2]) {
+        if (monto > aRepro[4] && monto <= aRepro[2]) {
           aRepro[4] = monto;//valor maximo de reproducciones
           aRepro[5] = i;//cancion con mayor numero de reproducciones
         }
@@ -43,18 +62,5 @@ function maxRepro(datos) {
   }
   return aRepro;
 }
-/*
-function max3Repro(datos) {
-  var masRepro = new Object();
-  masRepro.canR=0;
-  for (var i = 0; i < datos['canciones'].length ; i++) {
-    monto = datos['canciones'][i]['reproducciones']
-    if (monto > masRepro['canR']) {
 
-    }
-  }
-  return aRepro;
-
-}
-*/
 
