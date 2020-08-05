dd6810fb5a5eb5273b47c2479b9af54150b492fb
Victor Medina <vamedinac@icloud.com>
2020-08-04 22:47:16 -0500

    busca cancion modif dom

--- js/scriptC.js
+++ js/scriptC.js
@@ -2,32 +2,56 @@
 //buscar canciones
 function cargarCanciones() {
 
-$.ajax({
-  url: "http://127.0.0.1:5500/datos.json"
-}).done(function(todasCanciones){
-  
-  busca="hoy";
-  //console.log(todasCanciones);
-  
+  $.ajax({
+    url: "http://127.0.0.1:5500/datos.json"
+  }).done(function (todasCanciones) {
 
-  for (var i = 0; i < todasCanciones['canciones'].length; i++) {
+    busca = "vi";
+    //console.log(todasCanciones);
 
-    var concide=todasCanciones['canciones'][i]['nombre'];
-    console.log(concide);
+    for (var i = 0; i < todasCanciones['canciones'].length; i++) {
+
+      var nomCancion = todasCanciones['canciones'][i]['nombre'];
+      //console.log(nomCancion);
+
+      var canCoincide = nomCancion.indexOf(busca);
+      console.log(canCoincide);
+      
+      if (canCoincide != -1) {
+
+        //document.getElementById("tCanciones").innerHTML=
+        var resulCanciones = document.getElementById("tCanciones");
+        var newDiv = document.createElement("div"); //cada vez que recorra el for creamos un div nuevo
+        newDiv.className = "col col-md-4"
+        newDiv.innerHTML =
+        
+            "<div class='card'>"+
+                "<div class='card-header text-center'>"+
+                  "<img src='images/icon_"+(todasCanciones['canciones'][i]['icono'])+".svg' alt='logo 4' width='100'>"+
+                "</div>"+
+                "<div class='card-body'>"+
+                  "<h5 class='card-title text-center'>"+(todasCanciones['canciones'][i]['nombre'])+"</h5>"+
+                  "<audio src='songs/"+(todasCanciones['canciones'][i]['ruta'])+"' controls></audio>"+
+                "</div>"+
+            "</div>";
+           
+         
+        resulCanciones.appendChild(newDiv);
+        
+    }
     
-    while (concide==busca) {
-      console.log(concide)
-      busca="";
     }
 
-  }
-
-  var str = "Hello world, welcome to the universe.";
-  var buscarC = str.indexOf("e");
-  console.log(buscarC);
-
-});
+  });
 };
 
+//cargarCanciones();
 
-cargarCanciones();
+// boton buscar cancion
+$( document ).ready(function() {
+ 
+  $("button.buscar").click(function(){
+    cargarCanciones();
+  })
+
+});
