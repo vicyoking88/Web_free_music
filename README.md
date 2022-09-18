063f6966acab8c8c96cbe39abf9def5a2ee59dbd
Victor Medina <vmedina.qcode.ext@luegopago.com>
2022-09-17 19:30:42 -0500

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Web_free_music
+
 Web de musica gratis. utilizando HTML, CSS, Bootstrap Y Javascript.
+
+## Implementation
+
+-   instale la extension en tu VSCode `Live Server` para simular un servidor
+-   cambie la url del JSON con la lista de las canciones si asi lo requiere puerto por defecto `http://127.0.0.1:5500/datos.json`
+-   abrir en index.html presionar clic derecho en el mismo y escoger la opcion `Open with live server` automaticamente correra la aplicacion en tu navegador predeterminado.
+
+## Note
+
+-   puedes borrar agregar o cambiar las canciones de la carpeta `songs`, al igual que actualizarlas en el `datos.json` de acuerdo a sus necesidades o personalización.
--- js/scriptC.js
+++ js/scriptC.js
@@ -1,68 +1,61 @@
-
 //buscar canciones
 function cargarCanciones(buscando) {
-  limpiarBusqueda();
-  //busca="";
-  var busca = buscando.toLowerCase();
+	limpiarBusqueda();
+	//busca="";
+	var busca = buscando.toLowerCase();
 
-  console.log(busca);
+	console.log(busca);
 
-  $.ajax({
-    url: "http://127.0.0.1:5500/Web_free_music/datos.json"
-  }).done(function (todasCanciones) {
+	$.ajax({
+		url: "http://127.0.0.1:5500/datos.json",
+	}).done(function (todasCanciones) {
+		for (var i = 0; i < todasCanciones["canciones"].length; i++) {
+			var nomCancion = todasCanciones["canciones"][i]["nombre"];
 
-    
-    for (var i = 0; i < todasCanciones['canciones'].length; i++) {
+			console.log(nomCancion);
 
-      var nomCancion = todasCanciones['canciones'][i]['nombre'];
-    
-      console.log(nomCancion);
+			var canCoincide = nomCancion.indexOf(busca);
+			console.log(canCoincide);
 
-      var canCoincide = nomCancion.indexOf(busca);
-      console.log(canCoincide);
+			if (canCoincide != -1) {
+				//document.getElementById("tCanciones").innerHTML=
+				var resulCanciones = document.getElementById("tCanciones");
+				var newDiv = document.createElement("div"); //cada vez que recorra el for creamos un div nuevo
+				newDiv.className = "col-12 col-sm-6 col-md-4";
+				newDiv.innerHTML =
+					"<div class='card text-center'>" +
+					"<div class='card-header'>" +
+					"<img src='images/icon_" +
+					todasCanciones["canciones"][i]["icono"] +
+					".svg' alt='logo 4' width='100'>" +
+					"</div>" +
+					"<div class='card-body'>" +
+					"<h5 class='card-title'>" +
+					todasCanciones["canciones"][i]["nombre"] +
+					"</h5>" +
+					"<audio src='songs/" +
+					todasCanciones["canciones"][i]["ruta"] +
+					"' controls></audio>" +
+					"</div>" +
+					"</div>";
 
-      if (canCoincide != -1) {
-
-        //document.getElementById("tCanciones").innerHTML=
-        var resulCanciones = document.getElementById("tCanciones");
-        var newDiv = document.createElement("div"); //cada vez que recorra el for creamos un div nuevo
-        newDiv.className = "col-12 col-sm-6 col-md-4"
-        newDiv.innerHTML =
-
-          "<div class='card text-center'>" +
-          "<div class='card-header'>" +
-          "<img src='images/icon_" + (todasCanciones['canciones'][i]['icono']) + ".svg' alt='logo 4' width='100'>" +
-          "</div>" +
-          "<div class='card-body'>" +
-          "<h5 class='card-title'>" + (todasCanciones['canciones'][i]['nombre']) + "</h5>" +
-          "<audio src='songs/" + (todasCanciones['canciones'][i]['ruta']) + "' controls></audio>" +
-          "</div>" +
-          "</div>";
-
-
-        resulCanciones.appendChild(newDiv);
-
-      }
-
-    }
-    
-  });
-};
+				resulCanciones.appendChild(newDiv);
+			}
+		}
+	});
+}
 
 // entrada de busqueda
 i = 0;
-$(document).ready(function(){
-  $("input").keyup(function(){
-    cargarCanciones($(".algo").val());
-  });
+$(document).ready(function () {
+	$("input").keyup(function () {
+		cargarCanciones($(".algo").val());
+	});
 });
-
-
 
 // limpiar busqueda
 function limpiarBusqueda() {
-  document.getElementById("tCanciones").innerHTML = "<div></div>";
+	document.getElementById("tCanciones").innerHTML = "<div></div>";
 }
 
 cargarCanciones("");
-
